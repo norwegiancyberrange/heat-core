ae0177b530808372c5e1da98d8d01f2223441d8a
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-09-29 15:54:49 +0200

    Linting, removing unsued parameters, and adding bootscript for generic linux server

---
+++ lib/generic_boot.bash
@@ -0,0 +1,25 @@
+#!/bin/bash -v
+
+export DEBIAN_FRONTEND=noninteractive
+
+# These will be replace by openstack heat magic
+DOMAIN='<%DOMAIN%>'
+PUPPETSERVER='<%PUPPETSERVER%>'
+
+# Download deb for puppet repo
+tempdeb=$(mktemp /tmp/puppet.XXXXXX.deb) || exit 1
+wget https://apt.puppetlabs.com/puppet6-release-focal.deb -O "$tempdeb"
+dpkg -i "$tempdeb"
+
+# Update and install puppetserver
+apt update && apt -y upgrade
+apt install -y puppet-agent
+
+# Set FQDN
+echo "127.0.1.1 $(hostname).$DOMAIN $(hostname)" >> /etc/hosts
+
+# Configure puppet agent
+puppet='/opt/puppetlabs/bin/puppet'
+$puppet config set server $PUPPETSERVER --section main
+$puppet config set runinterval 600 --section main
+$puppet resource service puppet ensure=running enable=true
